5a66235b9c592c4e982ecdfbfb53a2cffd353350
Eder Sena <eder.sena@live.com>
2023-11-26 23:23:28 -0300

    Update package-lock.json and CSS styles

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,33 +1,41 @@
-import { PensamentoService } from './../pensamento.service';
-import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
+import { PensamentoService } from './../pensamento.service'
+import { Component, OnInit } from '@angular/core'
+import { Pensamento } from '../pensamento'
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css'],
 })
-export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos: Pensamento[] = [];
-  paginaAtual: number = 1;
-  haMaisPensamentos: boolean = true;
 
+export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos: Pensamento[] = []
+  paginaAtual: number = 1
+  haMaisPensamentos: boolean = true
+  filtro: string = ''
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos;
-    });
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
-      this.listaPensamentos.push(...listaPensamentos);
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos)
       if (listaPensamentos.length == 0) {
-        this.haMaisPensamentos = false;
+        this.haMaisPensamentos = false
       }
-      this.haMaisPensamentos = listaPensamentos.length > 0;
-    });
-  };
-}
+      this.haMaisPensamentos = listaPensamentos.length > 0
+    })
+  }
 
+  filtrarPensamentos() {
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
+}
